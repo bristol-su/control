1d983108d88247d4552b53821aebe8e3f497a9ca
Toby Twigger <tobytwigger1@gmail.com>
2019-11-27 09:43:42 +0000

    Fix bug with positoin name

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -114,8 +114,8 @@ class Role extends Model implements RoleContract
      */
     public function positionName(): string
     {
-        // TODO Isn't working
-        return $this->position_name;
+        // TODO Should return custom position name
+        return $this->position()->name();
     }
 
     /**
