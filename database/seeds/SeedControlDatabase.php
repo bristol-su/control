8760dacfc4bef64360b3aa0d8215a4635fade9bc
Toby Twigger <tt15951@my.bristol.ac.uk>
2020-01-24 13:35:54 +0000

    Update seeder

--- database/seeds/SeedControlDatabase.php
+++ database/seeds/SeedControlDatabase.php
@@ -1,5 +1,18 @@
 <?php
 
+use BristolSU\ControlDB\Models\Group;
+use BristolSU\ControlDB\Models\Position;
+use BristolSU\ControlDB\Models\Role;
+use BristolSU\ControlDB\Models\User;
+use BristolSU\ControlDB\Models\Tags\GroupTag;
+use BristolSU\ControlDB\Models\Tags\GroupTagCategory;
+use BristolSU\ControlDB\Models\Tags\PositionTag;
+use BristolSU\ControlDB\Models\Tags\PositionTagCategory;
+use BristolSU\ControlDB\Models\Tags\UserTag;
+use BristolSU\ControlDB\Models\Tags\UserTagCategory;
+use BristolSU\ControlDB\Models\Tags\RoleTag;
+use BristolSU\ControlDB\Models\Tags\RoleTagCategory;
+
 use Illuminate\Database\Seeder;
 
 class SeedControlDatabase extends Seeder
@@ -7,59 +20,58 @@ class SeedControlDatabase extends Seeder
 
     public function run()
     {
-        $users = factory(\BristolSU\ControlDB\Models\User::class, 100)->create();
-        $groups = factory(\BristolSU\ControlDB\Models\Group::class, 50)->create();
-        $positions = factory(\BristolSU\ControlDB\Models\Position::class, 10)->create();
+        // Create 100 users
+        $users = factory(User::class, 100)->create();
+        
+        // Create 50 groups
+        $groups = factory(Group::class, 50)->create();
+        
+        // Create 10 positions
+        $positions = factory(Position::class, 10)->create();
+        
+        // Give each group 10 users and each role in the group 1 or 2 users
         foreach($groups as $group) {
             $group->userRelationship()->attach($users->random(10));
-            /** @var \BristolSU\ControlDB\Models\Role[] $roles */
+            
             $roles = collect();
             foreach($positions->random(5) as $position) {
-                $roles->push(factory(\BristolSU\ControlDB\Models\Role::class)->create([
+                $roles->push(factory(Role::class)->create([
                     'group_id' => $group->id(),
                     'position_id' => $position->id()
                 ]));
             }
             foreach($roles as $role) {
-                $role->userRelationship()->attach($users->random(rand(1,2)), [
-                    'position_name' => 'somename'
-                ]);
+                $role->userRelationship()->attach($users->random(rand(1,2)));
             }
         }
         
-        $groupTagCategories = factory(\BristolSU\ControlDB\Models\Tags\GroupTagCategory::class, 15)->create();
-        foreach($groupTagCategories as $groupTagCategory) {
-            $groupTags = factory(\BristolSU\ControlDB\Models\Tags\GroupTag::class, 15)->create(['tag_category_id' => $groupTagCategory->id()]);
-            foreach($groupTags as $groupTag) {
+        // Tag groups
+        factory(GroupTagCategory::class, 15)->create()->each(function(GroupTagCategory $groupTagCategory) use ($groups) {
+            factory(GroupTag::class, 15)->create(['tag_category_id' => $groupTagCategory->id()])->each(function(GroupTag $groupTag) use ($groups) {
                 $groupTag->groupRelationship()->attach($groups->random(10));
-            }
-        }
+            });
+        });
 
-        $userTagCategories = factory(\BristolSU\ControlDB\Models\Tags\UserTagCategory::class, 15)->create();
-        foreach($userTagCategories as $userTagCategory) {
-            $userTags = factory(\BristolSU\ControlDB\Models\Tags\UserTag::class, 15)->create(['tag_category_id' => $userTagCategory->id()]);
-            foreach($userTags as $userTag) {
+        // Tag users
+        factory(UserTagCategory::class, 15)->create()->each(function(UserTagCategory $userTagCategory) use ($users) {
+            factory(UserTag::class, 15)->create(['tag_category_id' => $userTagCategory->id()])->each(function(UserTag $userTag) use ($users) {
                 $userTag->userRelationship()->attach($users->random(10));
-            }
-        }
+            });
+        });
 
+        // Tag roles
+        factory(RoleTagCategory::class, 15)->create()->each(function(RoleTagCategory $roleTagCategory) use ($roles) {
+            factory(RoleTag::class, 15)->create(['tag_category_id' => $roleTagCategory->id()])->each(function(RoleTag $roleTag) use ($roles) {
+                $roleTag->roleRelationship()->attach($roles->random(10));
+            });
+        });
 
-        $roleTagCategories = factory(\BristolSU\ControlDB\Models\Tags\RoleTagCategory::class, 15)->create();
-        foreach($roleTagCategories as $roleTagCategory) {
-            $roleTags = factory(\BristolSU\ControlDB\Models\Tags\RoleTag::class, 15)->create(['tag_category_id' => $roleTagCategory->id()]);
-            foreach($roleTags as $roleTag) {
-                $roleTag->roleRelationship()->attach(\BristolSU\ControlDB\Models\Role::all()->random(10));
-            }
-        }
-
-
-        $positionTagCategories = factory(\BristolSU\ControlDB\Models\Tags\PositionTagCategory::class, 15)->create();
-        foreach($positionTagCategories as $positionTagCategory) {
-            $positionTags = factory(\BristolSU\ControlDB\Models\Tags\PositionTag::class, 15)->create(['tag_category_id' => $positionTagCategory->id()]);
-            foreach($positionTags as $positionTag) {
-                $positionTag->positionRelationship()->attach($positions->random(4));
-            }
-        }
+        // Tag positions
+        factory(PositionTagCategory::class, 15)->create()->each(function(PositionTagCategory $positionTagCategory) use ($positions) {
+            factory(PositionTag::class, 15)->create(['tag_category_id' => $positionTagCategory->id()])->each(function(PositionTag $positionTag) use ($positions) {
+                $positionTag->positionRelationship()->attach($positions->random(10));
+            });
+        });
 
     }
     
